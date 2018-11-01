88a11162e4ddd21d8fe4a6f00b42785ee4f2edf5
Ryan Null <ryan.null@gmail.com>
2018-10-31 22:24:00 -0700

    ugh, it's supposed to be bytes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vatstat",
-    version="0.0.4",
+    version="0.0.5",
     author="Ryan Null",
     author_email="ryan.null@gmail.com",
     description="python api that makes interacting with the VATSIM data less painful and more pragmatic.",
